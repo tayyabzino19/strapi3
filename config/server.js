66e2a48f7ea12e4183ab4811ea4b0a746046aa96
tayyabzino19 <tayyab.zino19@gmail.com>
2021-11-23 09:36:56 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1d82785f17dc8a3845cc115696b7f75e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '05bd7cee-769f-46b1-8720-541d95ac4bc7'
+};
